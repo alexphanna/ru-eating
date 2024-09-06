a99954db45db08185622c37950eb98b6b85b2e59
alexphanna <ahanna0608@gmail.com>
2024-09-06 11:33:53 -0400

    Initial Commit

---
+++ MenRU/MenRUApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenRUApp.swift
+//  MenRU
+//
+//  Created by alex on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MenRUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
